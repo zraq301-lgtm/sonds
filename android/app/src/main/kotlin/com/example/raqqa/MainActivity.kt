95fe2ac45b09405c63123053ec6c6a560e5718a3
zraq301-lgtm <zraq301@gmail.com>
2026-01-28 02:55:40 +0200

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/raqqa/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.raqqa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // هذا الكود يربط تطبيقك بمحرك Flutter الحديث (v2) تلقائياً
+}
